07cb84a5ba3a7f584537b3a5d82730fd6b4a3e49
CiminoFrancesco <sfrancesco.cimino@itis.pr.it>
2023-09-21 21:18:30 +0200

    Esercizi JS

---
+++ Es Js/Ex Somma/script.js
@@ -0,0 +1,7 @@
+function Calcolo_Somma(){
+    var num_1 = parseInt(document.getElementById("primo").value);
+    var num_2 = parseInt(document.getElementById("secondo").value);
+
+    var somma = num_1 + num_2;
+    window.alert("La somma è " + somma);
+}
